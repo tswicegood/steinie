1fe76c12ad07cb0d405ee40d044fbb18615d179d
Travis Swicegood <development@domain51.com>
2014-07-26 13:51:02 -0500

    Add build stuff so we can have one release (v0.1.0)

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+if sys.argv[1] == 'develop':
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+setup(
+    name='steinie',
+    version='0.1.0',
+    author='Travis Swicegood',
+    author_email='development@domain51.com',
+    description='A little framework for doing web applications',
+    long_description=open("./README.rst").read(),
+    packages=['steinie', ],
+    include_package_data=True,
+    zip_safe=False,
+)
