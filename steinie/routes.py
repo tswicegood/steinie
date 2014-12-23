86d24257abeaec9883b8ce89e5a6961b5554e4ca
Travis Swicegood <development@domain51.com>
2014-12-23 14:24:44 -0800

    Slash some old code

--- steinie/routes.py
+++ steinie/routes.py
@@ -36,36 +36,3 @@ class Router(object):
 
             self.map.converters[name] = BasicParameter
         return outer
-
-
-class Route(object):
-    def __init__(self, path, methods):
-        self.path = path
-        self.methods = methods
-
-    def __lt__(self, request):
-        if not self.path_matches(request):
-            return False
-        if not self.is_allowed_method(request):
-            raise responses.Http405
-
-        request.route = self
-        self.request = request
-        return True
-
-    def path_matches(self, request):
-        return self.path == request.path
-
-    def is_allowed_method(self, request):
-        return request.method in self.methods
-
-
-def route(path, methods="GET"):
-    if not type(methods) is list:
-        methods = [methods, ]
-    return Route(path, methods)
-
-
-def get(path):
-    return route(path, methods=["GET"])
-
