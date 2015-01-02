9dc144d66cd0e1f007b959eea74157c3ec42cc99
Travis Swicegood <development@domain51.com>
2015-01-02 11:24:13 -0800

    Remove this old code

--- steinie/app.py
+++ steinie/app.py
@@ -1,7 +1,6 @@
 from werkzeug import serving
 from werkzeug import wrappers
 
-from . import responses
 from . import routes
 
 
@@ -18,19 +17,3 @@ class Steinie(routes.Router):
         host = "localhost"
         port = 5151
         serving.run_simple(host, port, self, use_debugger=True)
-
-
-def create_app(callback):
-    def app(environ, start_response):
-        request = wrappers.Request(environ)
-        try:
-            callback(request)
-
-            # `callback` should have bailed by now with a response, so
-            # we haven't found anything
-            response = responses.Http404()
-            return response.do(environ, start_response)
-
-        except responses.HttpResponse as response:
-            return response.do(environ, start_response)
-    return app
