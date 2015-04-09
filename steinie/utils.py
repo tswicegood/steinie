5cdee2146e0d55228c1172cb94bba9a73bfb47d2
Travis Swicegood <development@domain51.com>
2015-04-09 14:25:45 -0700

    Add wrapper to handle dispatching to routes and parameters

--- steinie/utils.py
+++ steinie/utils.py
@@ -1,6 +1,15 @@
 from functools import wraps
 
 
+def req_or_res(func):
+    def inner(request, response, **kwargs):
+        try:
+            return func(request, response, **kwargs)
+        except TypeError as e:
+            return func(request, **kwargs)
+    return inner
+
+
 def wrap_all_funcs(*funcs):
     next_func = None
     for func in reversed(funcs):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,6 +34,52 @@ class generate_next_TestCase(unittest.TestCase):
         self.assertEqual(wrapped.__name__, simple.__name__)
 
 
+class req_or_res_TestCase(unittest.TestCase):
+    def test_passes_request_and_response_if_both_are_available(self):
+        target = mock.Mock()
+        request = object()
+        response = object()
+        utils.req_or_res(target)(request, response)
+        target.assert_called_with(request, response)
+
+    def test_returns_result_from_wrapped_func(self):
+        r = random.randint(1000, 2000)
+        target = mock.Mock(return_value=r)
+        actual = utils.req_or_res(target)("some request", "some response")
+        self.assertEqual(actual, r)
+
+    def test_falls_back_to_request_only_if_response_not_available(self):
+        r = random.randint(1000, 2000)
+        target = mock.Mock()
+        target.side_effect = [TypeError(), r]
+        actual = utils.req_or_res(target)("some request", "some response")
+        self.assertEqual(actual, r)
+
+    def test_is_smart_enough_to_pass_other_args_as_well(self):
+        r = random.randint(100, 200)
+
+        def first(req, res, _next):
+            return _next(req, res)
+
+        second = utils.req_or_res(mock.Mock(side_effect=[TypeError(), r]))
+        wrapped = utils.wrap_all_funcs(first, second)
+        actual = wrapped("some request", "some response")
+        self.assertEqual(actual, r)
+
+    def test_wrapped_functions_are_given_all_the_args(self):
+        request = object()
+        response = object()
+
+        def first(req, res, _next):
+            return _next(req, res)
+
+        second = mock.Mock()
+        wrapped = [utils.req_or_res(f) for f in [first, second, mock.Mock()]]
+        utils.wrap_all_funcs(*wrapped)(request, response)
+
+        second.assert_called_with(request, response, _next=mock.ANY)
+
+
 class wrap_all_funcs_TestCase(unittest.TestCase):
     def test_wraps_list_of_functions(self):
         global call_count
