35aeadcb5b7ff0bc0e9e87eecead2bab10d621bf
Travis Swicegood <development@domain51.com>
2015-04-09 14:18:16 -0700

    Add in example using middleware and parameters

--- examples/params.py
+++ examples/params.py
@@ -7,6 +7,18 @@ from steinie import app, routing
 r = routing.Router()
 
 
+class AddUpperCaseMessage(object):
+    def __init__(self, app):
+        pass
+
+    def __call__(self, request, response, _next=None):
+        request.params["upper_msg"] = request.params["msg"].upper()
+        return _next(request, response)
+
+
+r.use(AddUpperCaseMessage)
+
+
 @r.param("msg")
 def to_upper(param):
     return param.upper()
@@ -15,6 +27,10 @@ def to_upper(param):
 @r.get("/<msg:msg>")
 def handler(request, response):
     return "Hello, {msg}".format(msg=request.params["msg"])
+
+@r.get("/upper/<string:msg>")
+def some_other_handler(request, response):
+    return "Hello, {upper_msg} or {msg}".format(**request.params)
 
 
 app = app.Steinie()
