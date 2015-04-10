7cb5fe918ad7f040c180565fa68a42fbfb85ef80
Travis Swicegood <development@domain51.com>
2015-04-10 03:06:09 -0400

    Fix missing dependency (v1.0.1)

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ else:
 
 setup(
     name='steinie',
-    version='1.0.0',
+    version='1.0.1',
     author='Travis Swicegood',
     author_email='development@domain51.com',
     description='A little framework for doing web applications',
     long_description=open("./README.rst").read(),
     packages=['steinie', ],
+    install_requires=[
+        'werkzeug>=0.10.1',
+    ],
     include_package_data=True,
     zip_safe=False,
 )
